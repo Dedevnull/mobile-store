033d1b52718fa7874ac45f4c5929d0af214d0e5b
David <117115474+Dedevnull@users.noreply.github.com>
2025-04-27 19:39:26 +0200

    Adapt fetchProducts to use cache utils and adapt its test suit

--- src/services/fetchProducts.js
+++ src/services/fetchProducts.js
@@ -1,10 +1,22 @@
+import { getCache, isCacheValid, setCache } from "../utils/cacheUtils";
+
 const VITE_API_URL = import.meta.env.VITE_API_URL;
+const CACHE_KEY = 'products_cache';
 
 export const fetchProducts = async () => {
+    const cachedData = getCache(CACHE_KEY);
+
+    if (cachedData && isCacheValid()) {
+        return cachedData.data;
+    }
+
     const response = await fetch(`${VITE_API_URL}/api/product`);
     if (!response.ok) {
         throw new Error('Failed to fetch products');
     }
     const data = await response.json();
+
+    setCache(CACHE_KEY, data);
+
     return data;
 };
--- tests/services/fetchProducts.test.js
+++ tests/services/fetchProducts.test.js
@@ -1,6 +1,13 @@
 import { afterEach, describe, test, vi } from 'vitest';
 import { fetchProducts } from '../../src/services/fetchProducts';
+import { getCache, isCacheValid, setCache } from '../../src/utils/cacheUtils';
 import { productsMockData } from '../mock/products';
+
+vi.mock('../../src/utils/cacheUtils', () => ({
+    getCache: vi.fn(),
+    setCache: vi.fn(),
+    isCacheValid: vi.fn(),
+}));
 
 afterEach(() => {
     vi.restoreAllMocks();
@@ -9,6 +16,10 @@ afterEach(() => {
 describe('Fetch Products:', () => {
     test('Should fetch and return products data when response is ok', async () => {
         const mockData = [...productsMockData];
+
+        getCache.mockReturnValue(null);
+        isCacheValid.mockReturnValue(false);
+
         vi.stubGlobal('fetch', vi.fn(() =>
             Promise.resolve({
                 ok: true,
@@ -17,8 +28,44 @@ describe('Fetch Products:', () => {
         ));
 
         const data = await fetchProducts();
+
         expect(data).toEqual(mockData);
         expect(fetch).toHaveBeenCalledWith(`${import.meta.env.VITE_API_URL}/api/product`);
+        expect(setCache).toHaveBeenCalledWith('products_cache', mockData);
+    });
+    
+    test('Should return products from cache if valid', async () => {
+        const cacheData = { timestamp: Date.now(), data: productsMockData };
+
+        getCache.mockReturnValue(cacheData);
+        isCacheValid.mockReturnValue(true);
+
+        const data = await fetchProducts();
+
+        expect(fetch).not.toHaveBeenCalled();
+        expect(setCache).not.toHaveBeenCalled();
+        expect(data).toEqual(productsMockData);
+
+    });
+
+    test('Should re-fetch products if cache is expired', async () => {
+        const expiredCacheData = { timestamp: Date.now() - 3600001, data: productsMockData }; // Expirado
+
+        getCache.mockReturnValue(expiredCacheData);
+        isCacheValid.mockReturnValue(false);
+
+        vi.stubGlobal('fetch', vi.fn(() =>
+            Promise.resolve({
+                ok: true,
+                json: () => Promise.resolve(productsMockData),
+            })
+        ));
+
+        const data = await fetchProducts();
+
+        expect(fetch).toHaveBeenCalledWith(`${import.meta.env.VITE_API_URL}/api/product`);
+        expect(setCache).toHaveBeenCalledWith('products_cache', productsMockData);
+        expect(data).toEqual(productsMockData);
     });
 
     test('Should throw an error when response is not ok', async () => {
