1afa005040399515cc04f857a6ee38093b1934ee
David <117115474+Dedevnull@users.noreply.github.com>
2025-04-27 22:11:54 +0200

    Implement useProduct on ProductDetailsPage

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { Loader } from '../components/Loader';
+import { useProduct } from '../hooks/useProduct';
+import { AddToCardButton } from '../components/AddToCardButton';
+import { ProductDetailDescription } from '../components/ProductDetailsDescription';
+import { ProductActions } from '../components/ProductActions';
 
 export const ProductDetailsPage = () => {
+  const { id } = useParams();  // Obtiene el id del producto desde la URL
+  const { product, loading, error } = useProduct(id);
+
+  if (loading) return <Loader />;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <div>
-      <h1>ProductDetailsPage</h1 >
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel qui autem, magni ducimus modi voluptatem maxime amet sapiente, praesentium unde rerum dolores dolore nihil illum, esse inventore aut dolor iusto.</p>
+    <div className='min-w-full px-5 bg-slate-50 flex flex-col xl:flex-row lg:flex-row justify-center'>
+      <div className="flex justify-center xl:w-1/2 lg:w-1/2">
+        <img src={product.imgUrl} alt={product.name} className="max-w-full max-h-[600px] object-contain" />
+      </div>
+      <div>
+        <ProductDetailDescription product={product} />
+        <div className='min-w-full mt-5'>
+          <ProductActions product={product} />
+        </div>
+      </div>
     </div>
   );
 };
