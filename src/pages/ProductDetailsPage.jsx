534159c1bff1ab8fae75c07446205cf9161a3386
David <117115474+Dedevnull@users.noreply.github.com>
2025-04-29 23:19:49 +0200

    Create a test suit for ProductDetailsPage
    - Testing loading screen
    - Testing Erro screen
    - Testing render product details

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -15,7 +15,7 @@ export const ProductDetailsPage = () => {
   return (
     <div className='min-w-full px-5 bg-slate-50 flex flex-col xl:flex-row lg:flex-row justify-center'>
       <div className="flex justify-center xl:w-1/2 lg:w-1/2">
-        <img src={product.imgUrl} alt={product.name} className="max-w-full max-h-[600px] object-contain" />
+        <img src={product.imgUrl} alt={`${product.brand}_${product.model}`} className="max-w-full max-h-[600px] object-contain" />
       </div>
       <div>
         <ProductDetailDescription product={product} />
---
+++ tests/pages/ProductDetailsPage.test.jsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, vi } from 'vitest';
+import { useProduct } from '../../src/hooks/useProduct';
+import { ProductDetailsPage } from '../../src/pages/ProductDetailsPage';
+
+vi.mock('../../src/hooks/useProduct', () => ({
+    useProduct: vi.fn(),
+}));
+
+describe("ProductDetailsPage", () => {
+    test('Should show loader when loading', () => {
+        useProduct.mockReturnValue({
+            product: null,
+            loading: true,
+            error: null,
+        });
+
+        render(<ProductDetailsPage />);
+
+        expect(screen.getByRole('status')).toBeInTheDocument();
+    });
+
+    test('Should shows error message when error exists', () => {
+        useProduct.mockReturnValue({
+            product: null,
+            loading: false,
+            error: "Failed to fetch product details",
+        });
+
+        render(<ProductDetailsPage />);
+
+        expect(screen.getByText('Error: Failed to fetch product details')).toBeInTheDocument();
+    });
+
+    test('Should show product details and actions when data is ready', () => {
+        const mockProduct = {
+            id: "ZmGrkLRPXOTpxsU4jjAcv",
+            brand: "Acer",
+            model: "Iconia Talk S",
+            imgUrl: "https://itx-frontend-test.onrender.com/images/ZmGrkLRPXOTpxsU4jjAcv.jpg",
+            options: { colors: [], storages: [] }
+        }
+
+        useProduct.mockReturnValue({
+            product: mockProduct,
+            loading: false,
+            error: null,
+        });
+
+        render(<ProductDetailsPage />);
+
+        const img = screen.getByRole('img');
+        expect(img).toHaveAttribute('src', mockProduct.imgUrl);
+        expect(img).toHaveAttribute('alt', `${mockProduct.brand}_${mockProduct.model}`);
+    });
+});
